dbb367366f03311f306f9a37994480343a8c580a
Tishlyy <ekaetteessien27@gmail.com>
2022-08-08 06:05:19 +0100

    Base test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -144,3 +144,17 @@ class TestBaseModelDocs(unttest.TestCase):
         inst = BaseModel()
         string = "[BaseModel] ({}) {}".format(inst.id, inst.__dict__)
         self.assertEqual(string, str(inst))
+
+    @mock.patch('models.storage')
+    def test_save(self, mock_storage):
+        """Test that save method updates `updated_at` and calls
+        `storage.save`"""
+        inst = BaseModel()
+        old_created_at = inst.created_at
+        old_updated_at = inst.updated_at
+        inst.save()
+        new_created_at = inst.created_at
+        new_updated_at = inst.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+        self.assertEqual(old_created_at, new_created_at)
+        self.assertTrue(mock_storage.save.called)
