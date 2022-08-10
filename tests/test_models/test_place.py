588fe4f80294ed34a2c2774223e4eaf4763aea0d
Tishlyy <ekaetteessien27@gmail.com>
2022-08-10 01:40:57 +0100

    Pycodestyle errors corrected

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -24,26 +24,26 @@ class TestPlaceDocs(unittest.TestCase):
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/place.py'])
         self.assertEqual(result.total_errors, 0,
-                          "Found code style errors (and warnings).")
+                         "Found code style errors (and warnings).")
 
     def test_pep8_conformity_test_place(self):
         """Test that tests/test_models/test_place.py conforms to PEP8"""
-        pep8s= pep8.styleGuide(quiet=True)
+        pep8s = pep8.styleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_models/test_place.py'])
         self.assertEqual(result.total_errors, 0,
-                        "Found code style errors (and warnings).")
+                         "Found code style errors (and warnings).")
 
     def test_place_module_docstring(self):
         """Test for place.py module docstring"""
         self.assertIsNot(place.__doc__, None,
-                        "place.py needs a docstring")
+                         "place.py needs a docstring")
         self.assertTrue(len(place.__doc__) >= 1,
                         "place.py needs a docstring")
 
     def test_place_class_docstring(self):
         """Test for place.py class docstring"""
         self.assertIsNot(Place.__doc__, None,
-                        "Place class needs a docstring")
+                         "Place class needs a docstring")
         self.assertTrue(len(Place__doc__) >= 1,
                         "Place class needs a docstring")
 
@@ -51,9 +51,10 @@ class TestPlaceDocs(unittest.TestCase):
         """Test for the docstring in Place methods"""
         for func in self.place_f:
             self.assertIsNot(func[1].__doc__, None,
-                            "{:s} method needs a docstring".format(func[0]))
+                             "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+
 
 class TestUser(unittest.TestCase):
     """Test for the Place class"""
